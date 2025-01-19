37f091a80c1b25d3ab0c12dd1c41ea802d58a9ed
mohsen <mohsen@gmail.com>
2025-01-19 12:30:47 +0200

    ui

--- lib/screens/DATA/data.dart
+++ lib/screens/DATA/data.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:zbooma/style/color.dart';
+import 'package:zbooma/style/padding.dart';
+import 'package:zbooma/style/size.dart';
 import 'package:zbooma/style/text.dart';
 
 class DATA extends StatefulWidget {
@@ -7,143 +10,350 @@ class DATA extends StatefulWidget {
 }
 
 class _DATAState extends State<DATA> {
-  final Map<String, String> data = {
-    'الاسم رباعي': '  ',
-    'رقم الهاتف': '  ',
-    'نوع الخدمة': '  ',
-    'نوع الباقة': '  ',
-    'اسم الموقع': '  ',
-    'رابط المنصة': '  ',
-    'نوع الهوست': '  ',
-    'الرقم القومي': '  ',
-    'السجل التجاري': '  ',
-    'IBAN': '  ',
-    'كلمة المرور': '  ',
-  };
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(104),
-        child: Container(
-          color: Colors.black,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              children: [
-                SizedBox(height: 30), // مسافة من الأعلى
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            'مرحبا بك!',
-                            textDirection: TextDirection.rtl,
-                            style: AppTextStyles.bodyText1,
-                          ),
-                          Text(
-                            'هنا تبدأ رحلتك نحو نجاح متجرك',
-                            textDirection: TextDirection.rtl,
-                            style: AppTextStyles.bodyText1,
-                          ),
-                        ],
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(104),
+          child: Container(
+            color: Colors.black,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 30), // مسافة من الأعلى
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'مرحبا بك!',
+                              textDirection: TextDirection.rtl,
+                              style: AppTextStyles.bodyText1,
+                            ),
+                            Text(
+                              'هنا تبدأ رحلتك نحو نجاح متجرك',
+                              textDirection: TextDirection.rtl,
+                              style: AppTextStyles.bodyText1,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ), // مسافة بين النصوص والصورة
-                    Container(
-                      margin:
-                          EdgeInsets.only(top: 20), // تعديل المسافة من الأعلى
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: Colors.green, width: 2),
+                      SizedBox(
+                        width: 20,
+                      ), // مسافة بين النصوص والصورة
+                      Container(
+                        margin:
+                            EdgeInsets.only(top: 20), // تعديل المسافة من الأعلى
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.green, width: 2),
+                        ),
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage('assets/datai.png'),
+                        ),
                       ),
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage('assets/datai.png'),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.asset(
-              'assets/data.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-          Positioned.fill(
-            child: SingleChildScrollView(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: data.entries
-                    .map((entry) => CustomDataRow(
-                          label: entry.key,
-                          value: entry.value,
-                        ))
-                    .toList(),
+        body: Stack(children: [
+          // Background image
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/data.png'),
+                fit: BoxFit.cover,
               ),
             ),
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class CustomDataRow extends StatelessWidget {
-  final String label;
-  final String value;
-
-  const CustomDataRow({required this.label, required this.value});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Expanded(
-                child: Text(
-                  value,
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.normal,
-                  ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: AppPadding.allMedium,
+              child: Align(
+                alignment: AlignmentDirectional.topEnd,
+                child: Column(
+                  children: [
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          " : الاسم رباعي  ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Container(
+                      color: AppColors.graay,
+                      height: AppSize.height(context, 1),
+                      width: AppSize.width(context, 300),
+                    ),
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          " : رقم الهاتف  ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Container(
+                      color: AppColors.graay,
+                      height: AppSize.height(context, 1),
+                      width: AppSize.width(context, 300),
+                    ),
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          " : نوع الخدمه   ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Container(
+                      color: AppColors.graay,
+                      height: AppSize.height(context, 1),
+                      width: AppSize.width(context, 300),
+                    ),
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          " : نوع الباقه   ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Container(
+                      color: AppColors.graay,
+                      height: AppSize.height(context, 1),
+                      width: AppSize.width(context, 300),
+                    ),
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          " : اسم الموقع   ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Container(
+                      color: AppColors.graay,
+                      height: AppSize.height(context, 1),
+                      width: AppSize.width(context, 300),
+                    ),
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          " : رابط المنصه   ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Container(
+                      color: AppColors.graay,
+                      height: AppSize.height(context, 1),
+                      width: AppSize.width(context, 300),
+                    ),
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          " : نوع الهوست   ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Container(
+                      color: AppColors.graay,
+                      height: AppSize.height(context, 1),
+                      width: AppSize.width(context, 300),
+                    ),
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          " :  الرقم القومي  ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Container(
+                      color: AppColors.graay,
+                      height: AppSize.height(context, 1),
+                      width: AppSize.width(context, 300),
+                    ),
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          " : السجل التجاري  ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Container(
+                      color: AppColors.graay,
+                      height: AppSize.height(context, 1),
+                      width: AppSize.width(context, 300),
+                    ),
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          " : IBAN  ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Container(
+                      color: AppColors.graay,
+                      height: AppSize.height(context, 1),
+                      width: AppSize.width(context, 300),
+                    ),
+                    // dddjhdjdhddddkdk
+                    SizedBox(
+                      height: AppSize.height(context, 15),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "",
+                          style: AppTextStyles.bodyText2,
+                        ),
+                        Text(
+                          "  :  كلمه المرور  ",
+                          style: AppTextStyles.bodyText1,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(width: 8),
-              Text(':$label', style: AppTextStyles.data),
-            ],
-          ),
-          SizedBox(height: 4),
-          Container(
-            width: double.infinity,
-            height: 1,
-            color: Colors.grey,
-          ),
-        ],
-      ),
-    );
+            ),
+          )
+        ]));
   }
 }
